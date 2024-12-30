d1c65b682df23968d7c6c6970be1e9571c61e9af
AlvaUtec <alvaro.garcia.h@utec.edu.pe>
2024-12-29 20:06:01 -0500

    Bird Implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,7 @@ import Bird from '../models/Bird'
 import Plane from '../models/Plane'
 
 
-{/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-</div> */}
+
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
@@ -44,6 +43,7 @@ const Home = () => {
 
   return (
     <section className='w-full h-screen relative'>
+      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'></div>
       <Canvas 
         className={`w-full h-screen bg-transparent 
           ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`
@@ -57,7 +57,9 @@ const Home = () => {
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
           
           <Bird />
-          <Sky />
+          <Sky 
+            isRotating={isRotating}
+          />
           <Island
             position={islandPosition}
             scale={islandScale}
